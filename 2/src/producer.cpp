76502f49b89dc6747d0e0857a10f4498c5a0e44b
dazzlemon <daanill13470@gmail.com>
2021-09-18 21:49:00 +0300

    finished doc

--- 2/src/producer.cpp
+++ 2/src/producer.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 void Producer::_main_loop() {
-    WaitForSingleObject(this->_semaphore_empty, INFINITE);// P(empty)
-    WaitForSingleObject(this->_semaphore_mutex, INFINITE);// P(mutex)
+    //WaitForSingleObject(this->_semaphore_empty, INFINITE);// P(empty)
+    //WaitForSingleObject(this->_semaphore_mutex, INFINITE);// P(mutex)
     this->_buffer->push(1);// produce item
     std::cout << "pushed" << std::endl;
     logger::log_buffer_state(*this->_buffer);
-    ReleaseSemaphore(this->_semaphore_mutex, 1, NULL);// V(mutex)
-    ReleaseSemaphore(this->_semaphore_full,  1, NULL);// V(full)
+    //ReleaseSemaphore(this->_semaphore_mutex, 1, NULL);// V(mutex)
+    //ReleaseSemaphore(this->_semaphore_full,  1, NULL);// V(full)
 }

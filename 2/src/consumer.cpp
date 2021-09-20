af1d22c11392d5b64780c606cdec46e0dca8dfae
dazzlemon <daanill13470@gmail.com>
2021-09-20 18:43:55 +0300

    uncommented semaphores

--- 2/src/consumer.cpp
+++ 2/src/consumer.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 void Consumer::_main_loop() {
-    //WaitForSingleObject(this->_semaphore_full,  INFINITE);// P(full)
-    //WaitForSingleObject(this->_semaphore_mutex, INFINITE);// P(mutex)
+    WaitForSingleObject(this->_semaphore_full,  INFINITE);// P(full)
+    WaitForSingleObject(this->_semaphore_mutex, INFINITE);// P(mutex)
     this->_buffer->pop();// consume item
     std::cout << "poped" << std::endl;
     logger::log_buffer_state(*this->_buffer);
-    //ReleaseSemaphore(this->_semaphore_mutex, 1, NULL);// V(mutex)
-    //ReleaseSemaphore(this->_semaphore_empty, 1, NULL);// V(empty)
+    ReleaseSemaphore(this->_semaphore_mutex, 1, NULL);// V(mutex)
+    ReleaseSemaphore(this->_semaphore_empty, 1, NULL);// V(empty)
 }

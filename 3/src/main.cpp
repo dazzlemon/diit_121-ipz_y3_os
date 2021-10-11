f10b7604459c41d6ac3dd1bbd134f8bbce77d462
dazzlemon <daanill13470@gmail.com>
2021-10-11 21:13:45 +0300

    unvectorized print_mbi because c++ is too wordy

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -2,37 +2,14 @@
 #include <windows.h>
 #include <vector>
 
-template<class T>
-std::string to_string(T t) {
-    return std::to_string(t);
-}
-
-std::string to_string(LPVOID& lpvoid) {
-    return std::to_string(reinterpret_cast<intptr_t>(lpvoid));
-}
-
 void print_mbi(MEMORY_BASIC_INFORMATION* mbi) {
-    std::vector<std::string> names = {
-        "PVOID  BaseAddress = ",
-        "PVOID  AllocationBase = ",
-        "DWORD  AllocationProtect = ",
-        "SIZE_T RegionSize = ",
-        "DWORD  State = ",
-        "DWORD  Protect = ",
-        "DWORD  Type = ",
-    };
-    std::vector<std::string> values = {
-        to_string(mbi->BaseAddress),
-        to_string(mbi->AllocationBase),
-        to_string(mbi->AllocationProtect),
-        to_string(mbi->RegionSize),
-        to_string(mbi->State),
-        to_string(mbi->Protect),
-        to_string(mbi->Type),
-    };
-    for (int i = 0; i < names.size(); i++) {
-        std::cout << "\t" << names[i] << values[i] << std::endl;
-    }
+    std::cout << "\tPVOID  BaseAddress = " << mbi->BaseAddress << std::endl;
+    std::cout << "\tPVOID  AllocationBase = " << mbi->AllocationBase << std::endl;
+    std::cout << "\tDWORD  AllocationProtect = " << mbi->AllocationProtect << std::endl;
+    std::cout << "\tSIZE_T RegionSize = " << mbi->RegionSize << std::endl;
+    std::cout << "\tDWORD  State = " << mbi->State << std::endl;
+    std::cout << "\tDWORD  Protect = " << mbi->Protect << std::endl;
+    std::cout << "\tDWORD  Type = " << mbi->Type << std::endl;
 }
 
 int main() {
@@ -42,5 +19,6 @@ int main() {
     MEMORY_BASIC_INFORMATION mbi;
     VirtualQuery(NULL, &mbi, sysinf.dwPageSize);
 
+    std::cout << "mbi = " << std::endl;
     print_mbi(&mbi);
 }

2483d292dddb8a4c9c105cbbb2e76526152eb89e
dazzlemon <daanill13470@gmail.com>
2021-10-11 20:32:18 +0300

    void print_mbi

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <windows.h>
+
+void print_mbi(MEMORY_BASIC_INFORMATION* mbi) {
+    std::cout << "PVOID  BaseAddress = " << mbi->BaseAddress << std::endl;
+    std::cout << "PVOID  AllocationBase = " << mbi->AllocationBase << std::endl;
+    std::cout << "DWORD  AllocationProtect = " << mbi->AllocationProtect << std::endl;
+    // std::cout << "WORD   PartitionId = " << mbi->PartitionId << std::endl;
+    std::cout << "SIZE_T RegionSize = " << mbi->RegionSize << std::endl;
+    std::cout << "DWORD  State = " << mbi->State << std::endl;
+    std::cout << "DWORD  Protect = " << mbi->Protect << std::endl;
+    std::cout << "DWORD  Type = " << mbi->Type << std::endl;
+}
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    SYSTEM_INFO sysinf;
+    GetSystemInfo(&sysinf);
+
+    MEMORY_BASIC_INFORMATION mbi;
+    VirtualQuery(NULL, &mbi, sysinf.dwPageSize);
+
+    print_mbi(&mbi);
 }

bb6b76def2427ed1c475d267e0894840a6141e07
dazzlemon <daanill13470@gmail.com>
2021-10-13 22:06:57 +0300

    new heap for overloaded new & delete operators

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -10,24 +10,43 @@
 
 #include "winapi_objects_cout.h"
 
+HANDLE heap;
+
+void heap_init() {
+    heap = HeapCreate(0, 0, 0);
+    if (!heap) {
+        throw new std::system_error(
+            GetLastError(), std::system_category(), "Error while trying to create new heap");
+    }
+}
 
 void* operator new(size_t size) {
-    auto result = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    // auto result = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    // if (result) {
+    //     return result;
+    // }
+    // throw new std::system_error(
+    //     GetLastError(), std::system_category(), "Error while trying to allocate memory");
+    
+    auto result = HeapAlloc(heap, 0, size);
     if (result) {
         return result;
     }
     throw new std::system_error(
-        GetLastError(), std::system_category(), "Error while trying to allocate memory");
+        -1, std::system_category(), "Error while trying to allocate memory");
 }
 
 void operator delete(void* p) {
-    if (!VirtualFree(p, 0, MEM_RELEASE)) {
+    // if (!VirtualFree(p, 0, MEM_RELEASE)) {
+    if (!HeapFree(heap, 0, p)) {
         throw new std::system_error(
             GetLastError(), std::system_category(), "Error while trying to release memory");
     }
 }
 
 int main() {
+    heap_init();
+
     SYSTEM_INFO sysinf;
     GetSystemInfo(&sysinf);
     print_sysinf("sysinf", sysinf);

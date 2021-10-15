d88792af6cc11f0eec03bc989402aa8270f25116
dazzlemon <daanill13470@gmail.com>
2021-10-15 20:50:24 +0300

    memory protection test done

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -44,15 +44,38 @@ void operator delete(void* p) {
     }
 }
 
-void test_memory_protection() {
-    // todo
+LPVOID find_page(const SYSTEM_INFO& sysinf, DWORD protection) {
+    LPVOID addr = NULL;
+    MEMORY_BASIC_INFORMATION mbi;
+    DWORD dword_max = std::numeric_limits<DWORD>::max();
+    for (
+        DWORD address = NULL, i = 0;
+        VirtualQuery(reinterpret_cast<LPVOID>(address), &mbi, sysinf.dwPageSize) && 
+            address <= dword_max && !addr;
+        address += mbi.RegionSize, i++
+    ) {
+        if (mbi.Protect == protection) {
+            addr = mbi.BaseAddress;
+            // print_mbi("mbi", mbi);
+        }
+    }
+    return addr;
+}
+
+void test_memory_protection(LPVOID addr) {
+    // read
+    int _ = *reinterpret_cast<int*>(addr);
+    std::cout << "read successfull" << std::endl;
+    // write
+    *reinterpret_cast<int*>(addr) = 3;
+    std::cout << "write successfull" << std::endl;
 }
 
 void test_granularity() {
     // todo
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     heap_init();
 
     SYSTEM_INFO sysinf;
@@ -66,15 +89,35 @@ int main() {
 
     MEMORY_BASIC_INFORMATION mbi;
     DWORD dword_max = std::numeric_limits<DWORD>::max();
+    DWORD pages_max = 20;
     for (
         DWORD address = NULL, i = 0;
         VirtualQuery(reinterpret_cast<LPVOID>(address), &mbi, sysinf.dwPageSize) && 
-            address <= dword_max && i < 20;
+            address <= dword_max && i < pages_max;
         address += mbi.RegionSize, i++
     ) {
         print_mbi("mbi" + std::to_string(i), mbi);
     }
 
-    test_memory_protection();
+    if (argc == 2) {
+        DWORD mp = 0x0;
+        int arg = std::stoi(argv[1]);
+        switch (arg) {
+            case 1: mp = PAGE_READONLY; break;
+            case 2: mp = PAGE_READWRITE; break;
+            case 3: mp = PAGE_NOACCESS; break;
+            default:
+                std::cout << "incorrect arg: " << argv[1]  << std::endl;
+                break;
+        }
+        if (mp != 0x0) {
+            std::cout << "testing page with protection: "
+                      << memory_protection_to_string(mp) << std::endl;
+            auto page_addr = find_page(sysinf, mp);
+            test_memory_protection(page_addr);
+        }
+    } else {
+        std::cout << "incorrect amount of args" << std::endl;
+    }
     test_granularity();
 }

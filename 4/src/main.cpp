5de1fc95330d306b0e48f7c2988bebbd9d90f33e
dazzlemon <daanill13470@gmail.com>
2021-11-11 22:44:36 +0200

    works now

--- 4/src/main.cpp
+++ 4/src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QDebug>
+#include <QListWidget>
 
 #include <windows.h>
 
@@ -10,7 +11,7 @@
 
 #define CHUNKSIZE 4
 #define ITERS 20
-#define SLEEPTIME 50
+#define SLEEPTIME 500
 
 HANDLE mutex;
 HANDLE empty;
@@ -18,6 +19,26 @@ HANDLE full;
 
 int counter = 0;
 std::vector<int> buffer;
+
+QApplication* a;
+MainWindow* w;
+
+void updateBufferList() {
+    QWidget* centralWidget = w->findChild<QWidget*>("centralwidget");
+    if (centralWidget == NULL) {
+        qDebug() << "centralWidget is NULL";
+        return;
+    }
+    QListWidget* bufferListWidget = centralWidget->findChild<QListWidget*>("bufferListWidget");
+    if (bufferListWidget == NULL) {
+        qDebug() << "bufferListWidget is NULL";
+        return;
+    }
+    bufferListWidget->clear();
+    for (auto i : buffer) {
+        bufferListWidget->addItem(QString::number(i));
+    }
+}
 
 void produce_item() {
     int number = ++counter;
@@ -36,6 +57,7 @@ DWORD __stdcall producer(void*) {
         WaitForSingleObject(mutex, INFINITE);
 
         produce_item();
+        updateBufferList();
 
         ReleaseSemaphore(mutex, 1, NULL);
 
@@ -56,14 +78,14 @@ DWORD __stdcall consumer(void*) {
         WaitForSingleObject(mutex, INFINITE);
 
         consume_item();
+        updateBufferList();
 
         ReleaseSemaphore(mutex, 1, NULL);
 
         if (chunk_i >= CHUNKSIZE) {
             chunk_i = 1;
             ReleaseSemaphore(empty, CHUNKSIZE, NULL);
-        }
-        else {
+        } else {
             chunk_i++;
         }
         Sleep(SLEEPTIME);
@@ -71,21 +93,29 @@ DWORD __stdcall consumer(void*) {
     return 0;
 }
 
+int argc_; char** argv_;
+
+DWORD __stdcall gui(void*) {
+    a = new QApplication(argc_, argv_);
+    w = new MainWindow();
+    w->show();
+    return a->exec();
+}
+
 int main(int argc, char* argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    argc_ = argc; argv_ = argv;
+    a = new QApplication(argc_, argv_);
+    w = new MainWindow();
 
     mutex = CreateSemaphore(NULL, 1, 1, NULL);
     empty = CreateSemaphore(NULL, CHUNKSIZE, CHUNKSIZE, NULL);
     full  = CreateSemaphore(NULL, 0, CHUNKSIZE, NULL);
 
-    HANDLE threads[2];
+    HANDLE threads[3];
 
-    threads[0] = CreateThread(NULL, 1024, producer, NULL, 0, NULL);
-    threads[1] = CreateThread(NULL, 1024, consumer, NULL, 0, NULL);
+    threads[0] = CreateThread(NULL, 1024, gui, NULL, 0, NULL);
+    threads[1] = CreateThread(NULL, 1024, producer, NULL, 0, NULL);
+    threads[2] = CreateThread(NULL, 1024, consumer, NULL, 0, NULL);
 
-    WaitForMultipleObjects(2, threads, true, INFINITE);
-
-    return a.exec();
+    WaitForMultipleObjects(3, threads, true, INFINITE);
 }

09077ad7f5bf738cc1a4ea5587f5f3af85c13bee
dazzlemon <daanill13470@gmail.com>
2022-03-20 22:30:03 +0200

    src done

--- 6/src/eval.cpp
+++ 6/src/eval.cpp
@@ -1,14 +1,22 @@
 #include <windows.h>
-#include <string>
 #include <iostream>
 #include "postfix_evaluator.hpp"
 #pragma hdrstop
 #pragma argsused
 
+// in windows all global vars are constructed just before DLL_PROCESS_ATTACH
+// and destructed right after DLL_PROCESS_DETACH
 PostfixEvaluator pe;
 
 int WINAPI DllMain(HINSTANCE hInst, DWORD fdwReason, PVOID pvReserved) {
-	std::cout << "DllMain\n";
+	switch (fdwReason) { 
+		case DLL_PROCESS_ATTACH:
+			std::cout << "DllMain process attached\n";
+			break;
+		case DLL_PROCESS_DETACH:
+			std::cout << "DllMain process detached\n";
+			break;
+	}
 	return TRUE;
 }
 

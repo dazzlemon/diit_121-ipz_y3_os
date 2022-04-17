d091b91e96eb1956622ec0e62ace9b418d10519b
dazzlemon <daanill13470@gmail.com>
2022-04-17 18:57:13 +0300

    final +-

--- 7/src/child.cpp
+++ 7/src/child.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <optional>
+#include <cstring>
 
 #include <signal.h>
 
@@ -7,6 +8,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+#define MESSAGE_LENGTH 256
 
 /**
  * returns name of the function that errored out
@@ -30,10 +33,40 @@ std::optional<std::string> writeToFile(
 	return {};
 }
 
-int main() {
-	char* text = "aboba";
-	writeToFile(
-		"test.txt", static_cast<void*>(text), sizeof(text) / sizeof(*text)
+int main(int argc, char* argv[]) {
+	/**
+	 * argv[1] - fileName
+	 * argv[2] - text(len <= MESSAGE_LENGTH)
+	 */
+
+	// verify argc
+	if (argc != 3) {
+		std::cout << "child process: Wrong amount of arguments, expected:\n"
+		          << "argv[1] - fileName\n"
+							<< "argv[2] - text(len <= MESSAGE_LENGTH)\n";
+		return -1;
+	}
+
+	// verify message length
+	auto textlen = strlen(argv[2]);
+	if (textlen > MESSAGE_LENGTH) {
+		std::cout << "child process: Wrong length of second argument \"text\":"
+		          	<< textlen << '\n'
+		          << "Should be <=MESSAGE_LENGTH\n";
+		return -1;
+	}
+
+	auto res = writeToFile(
+		argv[1], static_cast<void*>(argv[2]), strlen(argv[2])
 	);
-	int error = kill(getppid(), SIGUSR1);
+	if (res.has_value()) {
+		std::cout << "child process: " << res.value() << " returned -1\n"
+		          << "errno: " << errno << '\n';
+		return -1;
+	}
+
+	// signal to parent process
+	if (kill(getppid(), SIGUSR1) != 0) {
+		std::cout << "child process: kill error\n";
+	}
 }
--- 7/src/main.cpp
+++ 7/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <optional>
+#include <cstring>
 
 #include <signal.h>
 
@@ -8,6 +9,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+#define MESSAGE_LENGTH 256
+char* fileName;
 
 std::optional<std::string> readFromFile(
 	const char* fileName, void* str, size_t size
@@ -26,27 +30,65 @@ std::optional<std::string> readFromFile(
 }
 
 void signalHandler(int) {
-	char buffer[256];
-	readFromFile(
-		"test.txt", static_cast<void*>(buffer), sizeof(buffer) / sizeof(*buffer)
+	char buffer[MESSAGE_LENGTH];
+	auto res = readFromFile(
+		fileName, static_cast<void*>(buffer), sizeof(buffer) / sizeof(*buffer)
 	);
+	if (res.has_value()) {
+		std::cout << res.value() << " returned -1\n";
+		return;
+	}
 	std::cout << buffer << '\n';
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	/**
+	 * argv[1] - fileName
+	 * argv[2] - text(len <= MESSAGE_LENGTH)
+	 */
+
+	// verify argc
+	if (argc != 3) {
+		std::cout << "parent process: Wrong amount of arguments, expected:\n"
+		          << "argv[1] - fileName\n"
+							<< "argv[2] - text(len <= MESSAGE_LENGTH)\n";
+		return -1;
+	}
+
+	// verify message length
+	auto textlen = strlen(argv[2]);
+	if (textlen > MESSAGE_LENGTH) {
+		std::cout << "parent process: Wrong length of second argument \"text\":"
+		          	<< textlen << '\n'
+		          << "Should be <=MESSAGE_LENGTH\n";
+		return -1;
+	}
+
+	fileName = argv[1];
 	signal(SIGUSR1, signalHandler);
+
+	// create child process
 	int processId = fork();
 	if (processId == -1) {
-		// error
-	} else if (processId == 0) {// child process
-		if (execvp("bin/child", NULL) == -1) {
-			// TODO
-			std::cout << "execvp error\n";
-		}
-	} else {
-		int status;
-		if (waitpid(processId, &status, 0) == -1) {
-			// error
+		std::cout << "parent process: there was an error creating child process\n";
+		return -1;
+	}
+
+	// child process
+	if (processId == 0) {
+		char* argv_[]{"bin/child", argv[1], argv[2], NULL};
+		if (execvp("bin/child", argv_) == -1) {
+			std::cout << "child process: execvp error\n";
+			return -1;
 		}
 	}
+
+	// parent process
+	int status;
+	if (waitpid(processId, &status, 0) == -1) {
+		std::cout << "parent process: waitpid error\n";
+		return -1;
+	}
+
+	return 0;
 }

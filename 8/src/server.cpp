b7d1eea0edcdee77dd96741e454264e9c3207162
dazzlemon <daanill13470@gmail.com>
2022-05-04 23:38:23 +0300

    megaultrafinal

--- 8/src/server.cpp
+++ 8/src/server.cpp
@@ -23,11 +23,41 @@ std::string mode_tToString(mode_t p) {
 			 ;
 }
 
+#define PRINT_MESSAGE_QUEUE_STATE \
+	if (msgctl(messageQueueId, IPC_STAT, &messageQueueState) == -1) { \
+		std::cout << "error while getting message queue state (msgctl), errno:" \
+		          << errno << '\n'; \
+		return -1; \
+	} \
+	std::cout \
+		<< "message queue state:\n" \
+	  << "\townership and permissions:\n" \
+			<< "\t\tcreator user ID: "  << messageQueueState.msg_perm.cuid << '\n' \
+			<< "\t\tcreator group ID: " << messageQueueState.msg_perm.cgid << '\n' \
+			<< "\t\towner user ID: "  	<< messageQueueState.msg_perm.uid  << '\n' \
+			<< "\t\towner group ID: " 	<< messageQueueState.msg_perm.gid  << '\n' \
+			<< "\t\tpermissions: " \
+				<< mode_tToString(messageQueueState.msg_perm.mode) << '\n' \
+		<< "\ttime of last msgsnd: " \
+			<< time_tToString(messageQueueState.msg_stime) << '\n' \
+		<< "\ttime of last msgrcv: " \
+			<< time_tToString(messageQueueState.msg_rtime) << '\n' \
+		<< "\ttime of creation or last modification by msgctl: " \
+			<< time_tToString(messageQueueState.msg_ctime) << '\n' \
+		<< "\t# of bytes in queue: "    << messageQueueState.msg_cbytes << '\n' \
+		<< "\t# of messages in queue: "	<< messageQueueState.msg_qnum   << '\n' \
+    << "\tmaximum # of bytes in queue: " \
+			<< messageQueueState.msg_qbytes << '\n' \
+		<< "\tPID of last msgsnd: " << messageQueueState.msg_lspid << '\n' \
+		<< "\tPID of last msgrsv: " << messageQueueState.msg_lrpid << '\n';
+
 int main() {
 	std::cout << "Server started\n";
 	MY_IPC_KEY
 	int messageQueueId = msgget(myIpcKey, 0);
 	CHECK_MESSAGE_QUEUE_ID
+	msqid_ds messageQueueState;
+	PRINT_MESSAGE_QUEUE_STATE
 
 	Message message;
 	int result = msgrcv(
@@ -40,36 +70,5 @@ int main() {
 	CHECK_RESULT("receiving", "msgrcv")
 	std::cout << "received message(size = " << result << "):"
 	          << " \"" << message.string << "\"\n";
-
-	msqid_ds messageQueueState;
-	if (msgctl(messageQueueId, IPC_STAT, &messageQueueState) == -1) {
-		std::cout << "error while getting message queue state (msgctl), errno:"
-		          << errno << '\n';
-		return -1;
-	}
-	std::cout << "message queue state:\n"
-	          << "\townership and permissions:\n"
-							<< "\t\tcreator user ID: "
-								<< messageQueueState.msg_perm.cuid << '\n'
-							<< "\t\tcreator group ID: "
-								<< messageQueueState.msg_perm.cgid << '\n'
-							<< "\t\towner user ID: "
-								<< messageQueueState.msg_perm.uid << '\n'
-							<< "\t\towner group ID: "
-								<< messageQueueState.msg_perm.gid << '\n'
-							<< "\t\tpermissions: "
-								<< mode_tToString(messageQueueState.msg_perm.mode) << '\n'
-						<< "\ttime of last msgsnd: "
-							<< time_tToString(messageQueueState.msg_stime) << '\n'
-						<< "\ttime of last msgrcv: "
-							<< time_tToString(messageQueueState.msg_rtime) << '\n'
-						<< "\ttime of creation or last modification by msgctl: "
-							<< time_tToString(messageQueueState.msg_ctime) << '\n'
-						<< "\t# of bytes in queue: " << messageQueueState.msg_cbytes << '\n'
-						<< "\t# of messages in queue: "
-							<< messageQueueState.msg_qnum << '\n'
-            << "\tmaximum # of bytes in queue: "
-							<< messageQueueState.msg_qbytes << '\n'
-						<< "\tPID of last msgsnd: " << messageQueueState.msg_lspid << '\n'
-						<< "\tPID of last msgrsv: " << messageQueueState.msg_lrpid << '\n';
+	PRINT_MESSAGE_QUEUE_STATE
 }

6d3409f4cb1ce022e72257d4b37fefa0329192c7
dazzlemon <daanill13470@gmail.com>
2022-05-04 20:14:37 +0300

    almost done #8

--- 8/src/client.cpp
+++ 8/src/client.cpp
@@ -8,31 +8,49 @@
 #include "common.h"
 
 int main() {
-	std::cout << "Hello from client\n";
+	std::cout << "Client started\n";
+
+	key_t myIpcKey = ftok(".", 'a');
+	if (myIpcKey == -1) {
+		std::cout << "error while getting ipc key (ftok), errno: " << errno << '\n';
+		return -1;
+	}
+	std::cout << "ipc key: " << myIpcKey << '\n';
 
 	auto chmod = S_IRUSR | S_IWUSR
 	           | S_IRGRP | S_IWGRP
 	           | S_IROTH | S_IWOTH;
-
-	key_t myIpcKey = ftok(".", 'a');
 	int messageQueueId = msgget(myIpcKey, IPC_CREAT | chmod);
 	if (messageQueueId == -1) {
-		// TODO: error handle
+		std::cout << "error while getting message queue id (msgget), errno: "
+		          << errno << '\n';
 		return -1;
 	}
+	std::cout << "message queue id: " << messageQueueId << '\n';
 	
+	Message message;
+	message.type = 1;
+
 	std::cout << "please input 5 integers:\n";
-	int arr[5];
 	for (int i = 1; i <= 5; i++) {
-		std::cout << "\tinteger #" << i << ":";
-		std::cin >> arr[i - 1];
+		std::cout << "\tinteger #" << i << ": ";
+		std::cin >> message.array[i - 1];
 	}
 
-	Message message { 0, *arr };
-	int rec = msgsnd(
+	int result = msgsnd(
 		messageQueueId,
 		(struct msgbuf*) (&message),
 		sizeof(message.array),
 		0
 	);
+	if (result == -1) {
+		std::cout << "error while sending message (msgsnd), errno: "
+		          << errno << '\n';
+		return -1;
+	}
+	std::cout << "sent message:";
+	for (int i = 1; i <= 5; i++) {
+		std::cout << ' ' << message.array[i - 1];
+	}
+	std::cout << '\n';
 }
--- 8/src/server.cpp
+++ 8/src/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,6 +8,65 @@
 
 #include "common.h"
 
+std::string time_tToString(time_t time) {
+	tm *ltm = std::localtime(&time);
+	char mbstr[100];
+	std::strftime(mbstr, 100, "%d/%m/%Y %T", ltm);
+	return mbstr;
+}
+
 int main() {
-	std::cout << "Hello from server\n";
+	std::cout << "Server started\n";
+
+	key_t myIpcKey = ftok(".", 'a');
+	if (myIpcKey == -1) {
+		std::cout << "error while getting ipc key (ftok), errno: " << errno << '\n';
+		return -1;
+	}
+	std::cout << "ipc key: " << myIpcKey << '\n';
+
+	int messageQueueId = msgget(myIpcKey, 0);
+	if (messageQueueId == -1) {
+		std::cout << "error while getting message queue id (msgget), errno: "
+		          << errno << '\n';
+		return -1;
+	}
+	std::cout << "message queue id: " << messageQueueId << '\n';
+
+	Message message;
+	int result = msgrcv(
+		messageQueueId,
+		(struct msgbuf*) (&message),
+		sizeof(message.array),
+		1,
+		0
+	);
+	if (result == -1) {
+		std::cout << "error while receiving message (msgrcv), errno: "
+		          << errno << '\n';
+		return -1;
+	}
+	std::cout << "received message(size = " << result << "):";
+	for (int i = 1; i <= 5; i++) {
+		std::cout << ' ' << message.array[i - 1];
+	}
+	std::cout << '\n';
+
+	msqid_ds messageQueueState;
+	msgctl(messageQueueId, IPC_STAT, &messageQueueState);
+	std::cout << "message queue state:\n"
+	          << "\townership and permissions: TODO\n"
+						<< "\ttime of last msgsnd: "
+							<< time_tToString(messageQueueState.msg_stime) << '\n'
+						<< "\ttime of last msgrcv: "
+							<< time_tToString(messageQueueState.msg_rtime) << '\n'
+						<< "\ttime of creation or last modification by msgctl: "
+							<< time_tToString(messageQueueState.msg_ctime) << '\n'
+						<< "\t# of bytes in queue: " << messageQueueState.msg_cbytes << '\n'
+						<< "\t# of messages in queue: "
+							<< messageQueueState.msg_qnum << '\n'
+            << "\t maximum # of bytes in queue: "
+							<< messageQueueState.msg_qbytes << '\n'
+						<< "\t PID of last msgsnd: " << messageQueueState.msg_lspid << '\n'
+						<< "\t PID of last msgrsv: " << messageQueueState.msg_lrpid << '\n';
 }

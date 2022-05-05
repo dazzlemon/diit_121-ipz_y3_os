d142821010df9b8bc3f4723494b1bd1a0d5e3ad0
dazzlemon <daanill13470@gmail.com>
2022-05-06 00:44:41 +0300

    ftw - file tree walk, but also for the win

--- 9/src/main.cpp
+++ 9/src/main.cpp
@@ -3,63 +3,7 @@
 #include <sstream>
 #include <deque>
 
-#include <dirent.h>
-#include <sys/stat.h>
-
-#include <iostream>
-
-// #define ENABLE_DEBUG
-
-namespace debugging {
-#ifdef ENABLE_DEBUG
-	constexpr bool debug = true;
-#else
-	constexpr bool debug = false;
-#endif
-	template <typename... Args>
-	void print(const char* file, int line, Args... args) {
-			(std::clog << "[" << file << ":" << line << "] "
-								<< ... << args) << std::endl;
-	}
-}
-
-#define DEBUG(...)                                            \
-	do {                                                        \
-			if (debugging::debug) {                                 \
-					debugging::print(__FILE__, __LINE__, __VA_ARGS__);  \
-			}                                                       \
-	} while (0)
-
-char filetypeToChar(unsigned char d_type) {
-	switch (d_type) {
-		case DT_UNKNOWN: return 'u';
-		case DT_REG:     return 'f';
-		case DT_DIR:     return 'd';
-		case DT_FIFO:    return 'p';
-		case DT_SOCK:    return 's';
-		case DT_CHR:     return 'c';
-		case DT_BLK:     return 'b';
-		case DT_LNK:     return 'l';
-		// mostly bsd, but supported in XFS and CODA with kernel codes
-		case DT_WHT:     return 'w';
-		default:         return ' '; 
-	}
-}
-
-std::string opendirErrnoToString() {
-	switch (errno) {
-		// /var/db/sudo
-		// find in your system
-		// find / -type d ! -readable
-		case EACCES:  return "permission denied";
-		case EMFILE:  return "too many file descriptors in use by process";
-		case ENFILE:  return "too many files are currently opened in the system";
-		case ENOENT:  return "directory does not exist";
-		case ENOMEM:  return "insufficient memory to complete the operation";
-		case ENOTDIR: return "not a directory";
-		default: return "";
-	}
-}
+#include <ftw.h>
 
 std::string mode_tToString(mode_t p) {
 	return std::string(
@@ -73,6 +17,19 @@ std::string mode_tToString(mode_t p) {
 			 + ((p & S_IWOTH)  != 0 ? "w" : "-")
 			 + ((p & S_IXOTH)  != 0 ? "x" : "-")
 			 ;
+}
+
+char mode_tToChar(mode_t p) {
+	switch (p && S_IFMT) {
+		case S_IFREG:  return 'f';
+		case S_IFDIR:  return 'd';
+		case S_IFIFO:  return 'p';
+		case S_IFSOCK: return 's';
+		case S_IFCHR:  return 'c';
+		case S_IFBLK:  return 'b';
+		case S_IFLNK:  return 'l';
+		default:       return ' '; 
+	}
 }
 
 struct fileInfo {
@@ -90,6 +47,18 @@ std::string fileInfoVectorToString(std::vector<fileInfo> files) {
 								 << '\n';
 	}
 	return stringstream.str();
+}
+
+std::vector<fileInfo> files;
+int traverseFile(
+	const char* fpath, const struct stat* sb, int tflag, struct FTW* ftwbuf
+) {
+	files.push_back({
+		mode_tToString(sb->st_mode),
+		mode_tToChar(sb->st_mode),
+		fpath
+	});
+	return 0;
 }
 
 /**
@@ -127,70 +96,10 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	std::deque<std::string> directoryNames;
-	directoryNames.push_back(argv[1]);
-
-	std::vector<fileInfo> files;
-
-	do {// for all directories
-		bool continue_ = true;
-		
-		DIR* directory = opendir(directoryNames[0].c_str());
-		if (!directory) {
-			std::cout << "error: can't open directory \"" << argv[1] << "\": "
-								<< opendirErrnoToString() << '\n';
-			return -1;
-		}
-		DEBUG((std::string("directory ") + directoryNames[0] + " opened"));
-
-		int oldErrno;
-		do {// for all files
-			oldErrno = errno;
-			dirent* directoryEntry = readdir(directory);
-			if (directoryEntry) {
-				struct stat statbuf;
-				std::string filenameRelativeToPwd = directoryNames[0] + "/" +
-					directoryEntry->d_name;
-				if (stat(filenameRelativeToPwd.c_str(), &statbuf) == -1) {
-					std::cout << "error while trying to get info about file permissions "
-					          << "for file " << directoryEntry->d_name
-										<< ", errno: " << errno << '\n';
-					return -1;
-				}
-				if (std::string(directoryEntry->d_name) != "." &&
-				    std::string(directoryEntry->d_name) != ".."
-				) {
-					if (directoryEntry->d_type == DT_DIR) {
-						directoryNames.push_back(filenameRelativeToPwd);
-						DEBUG((
-							std::string("directory ") + filenameRelativeToPwd +
-							" added to directories list"
-						));
-					}
-					files.push_back({
-						mode_tToString(statbuf.st_mode),
-						filetypeToChar(directoryEntry->d_type),
-						filenameRelativeToPwd
-					});
-					DEBUG((
-						std::string("file ") + filenameRelativeToPwd + " added to file list"
-					));
-				}
-			} else {
-				continue_ = false;
-			}
-		} while (continue_);
-		if (errno != oldErrno) {
-			// may happen if someone deleted directory unexpectedly or something
-			// errno == EBADF // the only possible error
-			std::cout << "error while getting next directory entry (readdir)\n";
-			return -1;
-		}
-		closedir(directory);// TODO: check err
-		DEBUG((std::string("directory ") + directoryNames[0] + " closed"));
-		directoryNames.pop_front();
-	} while (!directoryNames.empty());
-	
+	if (nftw(argv[1], traverseFile, 20, 0) == -1) {
+    perror("nftw");
+    exit(EXIT_FAILURE);
+  }
 
 	std::cout << fileInfoVectorToString(files);
 }

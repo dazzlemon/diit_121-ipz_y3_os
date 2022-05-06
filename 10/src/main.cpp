fe7909e6437834e3763e25230287bd5ac03c2c37
dazzlemon <daanill13470@gmail.com>
2022-05-06 19:50:02 +0300

    seems like done

--- 10/src/main.cpp
+++ 10/src/main.cpp
@@ -12,9 +12,25 @@ int main() {
 		return -1;
 	}
 
-	if (tcsetattr(fd, 0, termios_p) == -1) {
+	// // Change control symbols
+	// termios_p->c_cc[VERASE] = VINTR;// VERASE = DEL
+	// termios_p->c_cc[VINTR] = VERASE;// VINTR = INT
+
+	// TODO: Change MIN, TIME in noncanonical mode
+	termios_p->c_lflag &= ICANON;
+	
+	termios_p->c_cc[VMIN] = 5;
+	termios_p->c_cc[VTIME] = 8;
+
+	if (tcsetattr(fd, TCSANOW, termios_p) == -1) {
 		std::cout << "error while setting stdin parameters, errno: "
 			<< errno << '\n';
 		return -1;
+	}
+
+	std::string string;
+	while (true) {
+		std::cin >> string;
+		std::cout << "read \"" << string << "\"\n";
 	}
 }

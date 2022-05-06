deab44d3d7e2a1fd6ecdabf7fea0c87a462e45df
dazzlemon <daanill13470@gmail.com>
2022-05-06 18:08:46 +0300

    tcgetattr, setattr

--- 10/src/main.cpp
+++ 10/src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+#include <termios.h>
+
 int main() {
-	std::cout << "Hello, World!\n";
+	termios* termios_p = new termios;
+	int fd = 0;// 0 - stdin
+
+	if (tcgetattr(fd, termios_p) == -1) {
+		std::cout << "error while getting stdin parameters, errno: "
+			<< errno << '\n';
+		return -1;
+	}
+
+	if (tcsetattr(fd, 0, termios_p) == -1) {
+		std::cout << "error while setting stdin parameters, errno: "
+			<< errno << '\n';
+		return -1;
+	}
 }

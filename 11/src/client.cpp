051aea1828273ba6e5655010193f388181de5001
dazzlemon <daanill13470@gmail.com>
2022-05-07 17:22:39 +0300

    message from cli

--- 11/src/client.cpp
+++ 11/src/client.cpp
@@ -4,7 +4,7 @@ int main() {
 	std::cout << "client started\n";
 	SOCKET_FILE_DESCRIPTOR
 	SOCKET_ADDRESS;
-	
+
 	auto result = connect(BIND_CONNECT_ARGS);
 	if (result == -1) {
 		std::cout << "error while initiating a connection on a socket, errno: "
@@ -13,14 +13,23 @@ int main() {
 	}
 	std::cout << "initiated socket connection\n";
 
-	char message[] = "aboba";// TODO
+	char message[256];
+	std::cout << "please input message: ";
+	std::string string;
+	std::getline(std::cin, string);
+	if (string.length() > 255) {
+		std::cout << "error: message too big to send\n";
+		return -1;
+	}
+	std::copy(string.begin(), string.end(), message);
+	message[string.length()] = '\0';
 
 	if (send(socketFileDescriptor, message, strlen(message), 0) == -1) {
 		std::cout << "error while sending message on a socket, errno: "
 		          << errno << '\n';
 		return -1;
 	}
-	std::cout << "sent message to socket\n";
+	std::cout << "sent message to socket: \"" << message << "\"\n";
 
 	if (close(socketFileDescriptor)) {
 		std::cout << "error while closing socket, errno: " << errno << '\n';

72cb958a85dd66f2fb32483b1a3b189750a9ce26
dazzlemon <daanill13470@gmail.com>
2022-05-07 19:36:55 +0300

    fix

--- 11/src/common.h
+++ 11/src/common.h
@@ -10,14 +10,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define SOCKET_FILE_DESCRIPTOR \
-	int socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0); \
-	if (socketFileDescriptor == -1) { \
-		std::cout << "error while creating endpoint for communication, errno: " \
-		          << errno << '\n'; \
+#define ASSERT(flag, errorWhile, done) \
+	if (flag == -1) { \
+		std::cout << "error while " << errorWhile << ", errno: " << errno << '\n'; \
 		return -1; \
 	} \
-	std::cout << "created endpoint for communication\n";
+	std::cout << done << '\n';
+
+#define SOCKET_FILE_DESCRIPTOR \
+	int socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0); \
+	ASSERT( socketFileDescriptor \
+	      , "creating endpoint for communication" \
+	      , "created endpoint for communication" \
+	      ) \
 
 #define SOCKET_ADDRESS \
 	sockaddr_in socketAddress { \
@@ -30,19 +35,11 @@
 	socketFileDescriptor, \
 	reinterpret_cast<sockaddr*>(&socketAddress)
 
-#define ASSERT(flag, errorWhile, done) \
-	if (flag == -1) { \
-		std::cout << "error while " << errorWhile << ", errno: " << errno << '\n'; \
-		return -1; \
-	} \
-	std::cout << done << '\n';
-
 #define BIND_CONNECT( \
 	fname, whileError, doneMessage \
 ) \
 	auto result = fname(SOCK_FD_AND_ADDR, sizeof(socketAddress)); \
-	ASSERT( \
-		result, \
-		whileError, \
-		doneMessage \
-	)
+	ASSERT(	result \
+	      , whileError \
+	      , doneMessage \
+	      )

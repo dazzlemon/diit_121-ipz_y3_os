07e1ac312ee4da0816fa3129daadf26e5143fcf0
dazzlemon <daanill13470@gmail.com>
2022-05-07 19:26:14 +0300

    macros use macros

--- 11/src/common.h
+++ 11/src/common.h
@@ -19,7 +19,8 @@
 	} \
 	std::cout << "created endpoint for communication\n";
 
-#define SOCKET_ADDRESS sockaddr_in socketAddress { \
+#define SOCKET_ADDRESS \
+	sockaddr_in socketAddress { \
 		.sin_family = AF_INET, \
 		.sin_port = htons(8080), \
 		.sin_addr = { .s_addr = inet_addr("127.0.0.1") } \
@@ -29,27 +30,19 @@
 	socketFileDescriptor, \
 	reinterpret_cast<sockaddr*>(&socketAddress)
 
-#define BIND_CONNECT_ARGS \
-	SOCK_FD_AND_ADDR, \
-	sizeof(socketAddress)
-	// TODO: add BIND_CONNECT macro
+#define ASSERT(flag, errorWhile, done) \
+	if (flag == -1) { \
+		std::cout << "error while " << errorWhile << ", errno: " << errno << '\n'; \
+		return -1; \
+	} \
+	std::cout << done << '\n';
 
 #define BIND_CONNECT( \
 	fname, whileError, doneMessage \
 ) \
-	auto result = fname( \
-		SOCK_FD_AND_ADDR, \
-		sizeof(socketAddress) \
-	); \
-	if (result == -1) { \
-		std::cout << "error while " << whileError << ", errno: " << errno << '\n'; \
-		return -1; \
-	} \
-	std::cout << doneMessage << '\n';
-
-#define ASSERT(flag, errorWhile, done) \
-	if (flag == -1) { \
-		std::cout << "error while" << errorWhile << ", errno" << errno << '\n'; \
-		return -1; \
-	} \
-	std::cout << done << '\n';
+	auto result = fname(SOCK_FD_AND_ADDR, sizeof(socketAddress)); \
+	ASSERT( \
+		result, \
+		whileError, \
+		doneMessage \
+	)

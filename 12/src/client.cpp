01876c2bcf610fffa08f8aa6a40f3aef43247610
dazzlemon <daanill13470@gmail.com>
2022-05-08 21:19:35 +0300

    save progress

--- 12/src/client.cpp
+++ 12/src/client.cpp
@@ -4,46 +4,52 @@
 #include <cstring>
 #include <netdb.h>
 
-int main() {
-	sockaddr_in* addrInf;
-	int s;
-	int rc;
+int main() {	
 	addrinfo hints;
-	addrinfo* addr;
-	char buf[1024];
-	
-	memset(&hints , 0, sizeof(hints));
+	memset(&hints, 0, sizeof(hints));
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_ADDRCONFIG;
-	rc = getaddrinfo("127.0.0.1", NULL, &hints, &addr);
-	if (rc) {
+	addrinfo* addr;
+	int result = getaddrinfo("127.0.0.1", NULL, &hints, &addr);
+	if (result) {
 		std::cout << "Сбой поиска имени хоста\n";// TODO: translate
-		return 1;
+		return -1;
 	}
 	
-	addrInf=(sockaddr_in*) addr->ai_addr;
-	s = socket(addrInf->sin_family,addr->ai_socktype,addr->ai_protocol);
-	if (s < 0) {
+	auto addrInf = reinterpret_cast<sockaddr_in*>(addr->ai_addr);
+	int socketFileDescriptor =
+		socket(addrInf->sin_family, addr->ai_socktype, addr->ai_protocol);
+	if (socketFileDescriptor < 0) {
 		std::cout << "Ошибка при создании сокета\n";// TODO: translate
-		return 1;
+		return -1;
 	}
 	
 	addrInf->sin_port = htons(1234);
-	if (connect(s, (sockaddr*) addrInf, addr->ai_addrlen)) {
+	if (connect(socketFileDescriptor, (sockaddr*) addrInf, addr->ai_addrlen)) {
 		std::cout << "Ошибка при подключении\n";// TODO: translate
-		return 1;
+		return -1;
 	}
 	
 	freeaddrinfo(addr);
 	std::cout << "---сообщение серверу\n";// TODO: translate
-	read(0, buf, sizeof(buf));
-	send(s, buf, sizeof(buf), 0);
-	if (recv(s,buf,sizeof(buf),0)<0) {
+	char buf[1024];
+	if (read(0, buf, sizeof(buf)) == -1) {
+		std::cout << "error while reading, errno: " << errno << '\n';
+		return -1;
+	} 
+
+	if (send(socketFileDescriptor, buf, sizeof(buf), 0) == -1) {
+		std::cout << "error while sending message to socket, errno: " << errno
+		          << '\n';
+		return -1;
+	}
+
+	if (recv(socketFileDescriptor, buf, sizeof(buf), 0) < 0) {
 		std::cout << "Ошибка при получении ответа\n";// TODO: translate
-		return 1;
+		return -1;
 	}
 	
-	std::cout << "---получен ответ от сервера\n%s",buf;// TODO: translate
-	close(s);
+	std::cout << "---получен ответ от сервера" << buf << '\n';// TODO: translate
+	close(socketFileDescriptor);
 	return 0;
 }
--- 12/src/server.cpp
+++ 12/src/server.cpp
@@ -12,8 +12,7 @@ pthread_mutex_t thread_busy_mutex;
 pthread_cond_t thread_flag_cv;
 int free_thr;
 int thread_flag;
-int i;
-int s;
+int socketFileDescriptor;
 int rc;
 void* buf1;
 
@@ -36,68 +35,75 @@ void* thread_func(void* thread_arg) {
 		while (!thread_flag) {
 			pthread_cond_wait(&thread_flag_cv, &thread_flag_mutex);
 			thread_flag = 0;
+			
 			pthread_mutex_lock(&thread_busy_mutex);
-			free_thr--;
+				free_thr--;
 			pthread_mutex_unlock(&thread_busy_mutex);
+			
 			pthread_mutex_unlock(&thread_flag_mutex);
-			service();
+				service();
 			pthread_mutex_lock(&thread_busy_mutex);
+			
 			free_thr++;
 			pthread_mutex_unlock(&thread_busy_mutex);
 		}
 	}
 }
 
-int main() {
-	sockaddr address;
-	size_t addrL = sizeof(address);
-	addrinfo hints;
-	addrinfo* addr;
-	sockaddr_in clt;
-	pthread_t id1;
-	pthread_t id2;
-	pthread_t id3;
+#define CREATE_THREAD(name) \
+	pthread_t name; \
+	pthread_create(&name, NULL, thread_func, NULL); \
 
+int main() {
 	pthread_mutex_init(&thread_flag_mutex, NULL);
 	pthread_mutex_init(&thread_busy_mutex, NULL);
-	pthread_cond_init(&thread_flag_cv, NULL);
+	pthread_cond_init( &thread_flag_cv,    NULL);
 	thread_flag = 0;
-	pthread_create(&id1, NULL, thread_func, NULL);
-	pthread_create(&id2, NULL, thread_func, NULL);
-	pthread_create(&id3, NULL, thread_func, NULL);
+
+	CREATE_THREAD(id1)
+	CREATE_THREAD(id2)
+	CREATE_THREAD(id3)
 	free_thr = 3;
+
+	addrinfo hints;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-	if ((rc = getaddrinfo(NULL, "1234", &hints, &addr))) {
+	addrinfo* addr;
+	rc = getaddrinfo(NULL, "1234", &hints, &addr);
+	if (rc) {
 		std::cout << "Сбой поиска имени хоста\n";// TODO: translate
 		return -1;
 	}
 	
-	if ((s = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol)) < 0) {
+	socketFileDescriptor =
+		socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+	if (socketFileDescriptor < 0) {
 		std::cout << "Ошибка при создании сокета\n";// TODO: translate
 		return -1;
 	}
 	
-	i = 1;
-	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i));
-	if (bind(s, addr->ai_addr, addr->ai_addrlen)) {
+	int optval = 1;// allow reuse of local addresses
+	setsockopt(
+		socketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (bind(socketFileDescriptor, addr->ai_addr, addr->ai_addrlen)) {
 		std::cout << "Ошибка при связывании сокета\n";// TODO: translate
 		return -1;
 	}
 	
 	freeaddrinfo(addr);
-	if (listen(s, 5)) {
+	if (listen(socketFileDescriptor, 5)) {
 		std::cout << "Ошибка listen\n";// TODO: translate
 		return -1;
 	}
-	
 	std::cout << "---cервер в ожидании\n";// TODO: translate
+
+	sockaddr socketAddress;
+	socklen_t socketLength = sizeof(socketAddress);
 	while (true)	{
-		bzero(&clt, sizeof(clt));
-		conn = accept( s
-		             , reinterpret_cast<sockaddr*>(&address)
-		             , reinterpret_cast<socklen_t*>(&addrL)
+		conn = accept( socketFileDescriptor
+		             , reinterpret_cast<sockaddr*>(&socketAddress)
+		             , (&socketLength)
 		             );
 		if (conn < 0) {
 			return -1;
